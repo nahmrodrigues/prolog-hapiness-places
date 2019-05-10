c161a9e1f14cf39ba228a19b342dcaf06d355cab
Ícaro Pires <icaropsa@gmail.com>
2019-05-10 00:28:56 -0300

    Make generate_all_databases a script

--- parser/generate_all_databases.py
+++ parser/generate_all_databases.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
+
 import pandas as pd
-from os import path
+from os import path, makedirs
 
 from convert_to_prolog import convert_to_prolog
 from databases_specs import databases
 
-def generate_base(database, src_path='../assets', dest_path='../knowledge_bases'):
+BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
+def generate_base(database, src_path=path.join(BASE_DIR, 'assets'), dest_path=path.join(BASE_DIR, 'knowledge_databases')):
     df = pd.read_csv(path.join(src_path, database.original_base + '.csv'), dtype='unicode')
 
     if database.query:
@@ -13,14 +17,15 @@ def generate_base(database, src_path='../assets', dest_path='../knowledge_bases'
     prolog_database = convert_to_prolog(df, database.columns, database.fact_name)
 
     out_filename = path.join(dest_path, database.name + '.pl')
+    makedirs(dest_path, exist_ok=True)
     with open(out_filename, 'w') as f:
         f.writelines(prolog_database)
 
 if __name__ == '__main__':
-    print('Generating bases...\n')
+    print('Generating databases...\n')
 
     for database in databases:
         generate_base(database)
-        print("Generated base '{}'".format(database.name))
+        print("Generated database '{}'".format(database.name))
 
-    print('\nGenerated bases:', ', '.join(database.name for database in databases))
+    print('\nGenerated databases:', ', '.join(database.name for database in databases))
